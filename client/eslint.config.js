d04ba9e844af23de2baea17798befbdf28e9f165
yolandaatt <yolandaatteyeh3@gmail.com>
2025-08-27 19:41:34 +0200

    skapat frontend för Vite projekt med TS och TailWind, installerat ESLint, Prettier, Husky, Frontend CI och gitHub Actions workflow

---
+++ client/eslint.config.js
@@ -0,0 +1,43 @@
+// client/eslint.config.js
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      },
+      globals: globals.browser
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y
+    },
+    rules: {
+      // React 17+ behöver inte import React
+      'react/react-in-jsx-scope': 'off',
+
+      // Hooks regler
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
+  },
+
+  // Prettier sist, stänger av regler som krockar
+  eslintConfigPrettier
+];
